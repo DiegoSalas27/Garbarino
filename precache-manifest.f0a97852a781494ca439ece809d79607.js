87e157cfcf6dc58c7336ed983856ed554016ac27
Diego Salas Noain <diegosalasnoain@Diegos-MacBook-Pro.local>
2019-03-08 01:48:19 -0500

    Updates

---
+++ precache-manifest.f0a97852a781494ca439ece809d79607.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7ab449242c309ab2b3f",
+    "url": "/Garbarino/static/js/runtime~main.23d98299.js"
+  },
+  {
+    "revision": "27b21b6d875880243e07",
+    "url": "/Garbarino/static/js/main.896052ed.chunk.js"
+  },
+  {
+    "revision": "b67f1459197dc0e9f4c2",
+    "url": "/Garbarino/static/js/2.0e160fe8.chunk.js"
+  },
+  {
+    "revision": "27b21b6d875880243e07",
+    "url": "/Garbarino/static/css/main.c02d87a3.chunk.css"
+  },
+  {
+    "revision": "b67f1459197dc0e9f4c2",
+    "url": "/Garbarino/static/css/2.cd7b0a6e.chunk.css"
+  },
+  {
+    "revision": "b59ed893dd8f0ad9eab63439f24b198d",
+    "url": "/Garbarino/index.html"
+  }
+];
